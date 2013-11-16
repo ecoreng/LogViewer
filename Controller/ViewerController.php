145a03ff38eae48772523d32b3821028c9021a04
ecoreng <ecor.rck@hotmail.com>
2013-11-15 19:15:47 -0800

    fix redirect when there is no log

--- Controller/ViewerController.php
+++ Controller/ViewerController.php
@@ -2,33 +2,69 @@
 
 App::uses('LogViewerAppController', 'LogViewer.Controller');
 
+class ViewerController extends LogViewerAppController
+{
 
-class ViewerController extends LogViewerAppController {
-	
-	public $logPath = '';
+    public $logPath = '';
+    public $cachePrefix = array('cake_', 'croogo_');
 
-    public function beforeFilter() {
+    public function beforeFilter()
+    {
         parent::beforeFilter();
-        $this->logPath =  APP . 'tmp' . DS . 'logs' . DS . 'error.log';
+        $this->logPath = APP . 'tmp' . DS . 'logs' . DS . 'error.log';
     }
-    
-	public function admin_index() {
-		$this->set('title_for_layout', 'Log Viewer');
-		$flcnt = '';
-		if(is_readable($this->logPath)) {
-			$flcnt = file_get_contents($this->logPath);
-			$flcnt = explode('{main}' . PHP_EOL, $flcnt);
-		}	
-		$this->set('logs', $flcnt);
-	}
 
-	public function admin_kill() {
-		if(is_readable($this->logPath)) {
-			unlink($this->logPath);
-			$this->redirect(
-				array('action' => 'index'		
-			));
-		}
-	}
+    public function admin_index()
+    {
+        $this->set('title_for_layout', 'Log Viewer');
+        $flcnt = '';
+        if (is_readable($this->logPath)) {
+            $flcnt = file_get_contents($this->logPath);
+            $flcnt = explode('{main}' . PHP_EOL, $flcnt);
+        }
+        $this->set('logs', $flcnt);
+    }
+
+    public function admin_kill()
+    {
+        if (is_readable($this->logPath)) {
+            @unlink($this->logPath);
+        }
+        $this->redirect(
+                array(
+                    'plugin' => 'LogViewer',
+                    'controller' => 'Viewer',
+                    'action' => 'index',
+                    'admin' => true,
+        ));
+    }
+
+    public function admin_clearcache()
+    {
+        $this->delete();
+    }
+
+    protected function delete($path = '', $recursive = true)
+    {
+        if (!$path) {
+            $path = CACHE;
+        }
+        $dirItems = scandir($path);
+        $ignore = array('.', '..');
+        foreach ($dirItems AS $dirItem) {
+            if (in_array($dirItem, $ignore)) {
+                continue;
+            }
+
+            if (is_dir($path . $dirItem) && $recursive) {
+                $this->delete($path . $dirItem . DS);
+            } elseif (substr($dirItem, 0, 5) == 'cake_' || substr($dirItem, 0, 7) == 'croogo_') {
+                unlink($path . $dirItem);
+            }
+        }
+        $this->redirect(
+                array('action' => 'index'
+        ));
+    }
 
 }
--- View/Viewer/admin_index.ctp
+++ View/Viewer/admin_index.ctp
@@ -5,7 +5,10 @@ $this->Html
 	->addCrumb('Log Viewer', array('controller' => 'Viewer', 'action' => 'index'));
 
 echo $this->Html->link('Delete Log', array('action' => 'kill') , array('class' => 'btn'));
+echo $this->Html->link('Clear Cache', array('action' => 'clearcache') , array('class' => 'btn'));
+
 echo '<div></div>';
+
 if (is_array($logs)) {
 if (count($logs) > 0) {
 krsort($logs);
